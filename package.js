b2456f8905ba627fdbf5cd51f792493f9f06b797
Herrhelms <dev@straw.io>
2015-08-13 19:13:21 +0000

    name changes

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-    name: 'herrhelms:meteor-vcard',
+    name: 'herrhelms:meteor-vcard-dummy',
     version: '0.0.1',
-    summary: 'Serve .vcf files for download within your meteor app',
-    git: 'https://github.com/herrhelms/meteor-vcards.git',
+    summary: 'Playground package to generate and download .vcf files within your meteor app',
+    git: 'https://github.com/herrhelms/meteor-vcards-dummy.git',
     documentation: 'README.md'
 });
 
@@ -10,5 +10,5 @@ Package.onUse(function(api) {
     api.versionsFrom('1.1.0.2');
     api.imply('iron:router@1.0.7');
     api.use(['templating', 'spacebars', 'reactive-var'], 'client');
-    api.addFiles('meteor-vcards.js');
+    api.addFiles('meteor-vcard-dummy.js');
 });
